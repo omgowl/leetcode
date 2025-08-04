d94b190422fb51b40cfc8ee5a2dd4419c1ad8402
pmkaboo <peter@owltech.wtf>
2025-08-04 22:58:28 +0200

    Add option to run specific test case

--- tests/acceptance.rb
+++ tests/acceptance.rb
@@ -4,10 +4,21 @@ require_relative "../#{ARGV[0]}/tests/acceptance"
 class TestAcceptance < Minitest::Test
   def test_acceptance
     Acceptance.solutions.each do |solution|
-      Acceptance.test_cases.each.with_index do |test_case, idx|
-        assert_equal test_case[:expected], solution[:lambda].call(test_case[:args]),
-                     "Failed `#{solution[:name]}` case: #{idx + 1}"
+      if ARGV[1]
+        idx = ARGV[1].to_i - 1
+        run_test_case(solution, Acceptance.test_cases[idx], idx)
+      else
+        Acceptance.test_cases.each.with_index do |test_case, idx|
+          run_test_case(solution, test_case, idx)
+        end
       end
     end
+  end
+
+  private
+
+  def run_test_case(solution, test_case, idx)
+    assert_equal test_case[:expected], solution[:lambda].call(test_case[:args]),
+                 "Failed `#{solution[:name]}` case: #{idx + 1}"
   end
 end
